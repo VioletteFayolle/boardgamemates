50f0d4f542f2e2f8e3a33f4b88f3c22f05ed0aef
Arnaud Floriani <sarkraf.dev@gmail.com>
2024-08-21 16:32:28 +0200

    add minimum style and content for event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,8 +1,8 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User', foreign_key: 'user_id'
 
-  validates :title, presence: true
-  validates :description, presence: true
+  validates :title, presence: true, uniqueness: true
+  validates :description, presence: true, length: { minimum: 10 }
   validates :location, presence: true
   validates :date, presence: true
   validates :capacity, presence: true, numericality: { only_integer: true, greater_than: 0 }
