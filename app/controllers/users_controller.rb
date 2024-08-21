3e28762a4c77ad511eb04269547adaa77e7573d1
Arnaud Floriani <sarkraf.dev@gmail.com>
2024-08-21 10:42:50 +0200

    reset config.action_controller.raise_on_missing_callback_actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[show]
-  skip_after_action :verify_authorized, only: %i[show]
+
   def show
     @user = User.find_by(username: params[:username])
   end
