e91ac5158bfb662a8773747746c8341e9d0d5743
Arnaud Floriani <sarkraf.dev@gmail.com>
2024-08-24 16:07:24 +0200

    add photo to user and display it in user/show

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_one_attached :photo
+
   has_many :my_events, class_name: 'Event', foreign_key: 'user_id', dependent: :destroy
 
   has_many :inscriptions, dependent: :destroy
@@ -14,4 +16,23 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  after_create :generate_initials_image
+
+  def generate_initials_image
+    image = Cloudinary::Uploader.upload("https://via.placeholder.com/150x150.png?text=#{initials}",
+                                        public_id: "user_#{id}_initials",
+                                        transformation: [
+                                          { width: 150, height: 150, gravity: "center", crop: "fill" },
+                                          { overlay: "text:Arial_60_bold:#{initials}", gravity: "center", color: "white" }
+                                        ])
+
+    self.update(image_url: image['secure_url'])
+  end
+
+  private
+
+  def initials
+    "#{first_name[0].upcase}#{last_name[0].upcase}"
+  end
 end
